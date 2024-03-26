24c3a0a1fed6df0a025ee033e6cfd20f5bd668fd
Federico <Fedengp@gmail.com>
2024-03-26 11:43:14 -0300

    fix: language working now

--- codimixapp/static/js/main.js
+++ codimixapp/static/js/main.js
@@ -20,27 +20,23 @@ function copyToClipboard() {
 document.getElementById('submit-button').addEventListener('click', input_to_code);
 
 function input_to_code() {
-    if (document.getElementById('user_input').value !== '' && document.getElementById('language-select').value !== 'sela') {
-        //receive user input
-        const text = document.getElementById('user_input').value;
-        fetch(`/process_user_input/?user_input=${encodeURIComponent(text)}`)
+    const user_input = document.getElementById('user_input').value;
+    const selected_language = document.getElementById('language-select').value;
+
+    if (user_input !== '' && selected_language !== 'sela') {
+        fetch(`/process_user_input/?user_input=${encodeURIComponent(user_input)}&selected_language=${encodeURIComponent(selected_language)}`)
             .then(response => response.json())
             .then(data => {
                 document.getElementById('textToCopy').innerText = data.user_sent;
             })
             .catch(error => console.error('Error:', error));
-        //restarts input field    
+
         document.getElementById('user_input').value = '';
+    } else {
+        alert('Please enter text and select a language');
+    }
+}
 
-        //receive language
-        const selectedLanguage = document.getElementById('language-select').value;
-        console.log('selected language: ', selectedLanguage);
-        fetch(`/process_user_input/?language_selected=${encodeURIComponent(selectedLanguage)}`)
-            .catch(error => console.error('Error:', error));
-
-    } else{
-        alert('Please enter text and select a language')
-}};
 
 var user_input = document.getElementById("user_input");
 
